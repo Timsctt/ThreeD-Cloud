249390df3d1cb617365c6b36e2342e279acad6c4
Timsht <38407598+Timsht@users.noreply.github.com>
2023-02-19 11:56:09 +0100

    refactor: move logic component in dedicated hook

--- src/components/ThreeDCloud/ThreeDCloud.tsx
+++ src/components/ThreeDCloud/ThreeDCloud.tsx
@@ -1,6 +1,6 @@
-import React, { useState, useLayoutEffect, Fragment } from 'react';
+import React, { Fragment } from 'react';
 import { useThreeDCloud } from '../../hooks/useThreeDCloud';
-import { CloudProps, Position } from '../types';
+import { CloudProps } from '../types';
 import CloudElement from './CloudElement';
 
 import './style.css';
@@ -11,77 +11,12 @@ const ThreeDCloud: React.FunctionComponent<CloudProps> = ({
   size,
   speed,
 }: CloudProps) => {
-  const elements: Array<JSX.Element> = React.Children.toArray(
-    children
-  ) as Array<JSX.Element>;
-
-  const [hasLoaded, setHasLoaded] = useState<boolean>(false);
-  const [elementsList, setElementsList] = useState<Array<CloudProps>>(
-    elements.map((element) => ({
-      item: element!.props.children,
-      radius,
-      size,
-      speed,
-    }))
-  );
-  const { mounted, sc, depth } = useThreeDCloud({ size, speed, radius });
-
-  const elementRandomPosition: Array<Number> = elements.map(
-    (_element, index) => index
-  );
-
-  function createItems() {
-    const cloudElements = elementsList.map((element, index) => ({
-      item: element.item,
-      radius,
-      position: computePosition(index),
-      size,
-      speed,
-    }));
-
-    setElementsList(cloudElements);
-    setHasLoaded(true);
-  }
-
-  function computePosition(index: number, random = true): Position {
-    const textsLength = elements.length;
-
-    if (random)
-      index = getRandomPosition(
-        Math.floor(Math.random() * (textsLength + 1))
-      ).valueOf();
-    const theta = Math.acos(-1 + (2 * index + 1) / textsLength);
-    const phi = Math.sqrt((textsLength + 1) * Math.PI) * theta;
-
-    return {
-      x: (size * Math.sin(theta) * Math.cos(phi)) / 2,
-      y: (size * Math.sin(theta) * Math.sin(phi)) / 2,
-      z: (size * Math.cos(theta)) / 2,
-    };
-  }
-
-  function getRandomPosition(index: number): Number {
-    const closest = elementRandomPosition.reduce(
-      (previousValue: Number, currentValue: Number): Number =>
-        Math.abs(currentValue.valueOf() - index) <
-        Math.abs(previousValue.valueOf() - index)
-          ? currentValue
-          : previousValue
-    );
-    for (let i = 0; i < elementRandomPosition.length; i++) {
-      if (elementRandomPosition[i] === closest) {
-        elementRandomPosition.splice(i, 1);
-      }
-    }
-    return closest;
-  }
-
-  useLayoutEffect(() => {
-    if (mounted.current) {
-      createItems();
-      mounted.current = false;
-    }
-  }, []);
+  const { sc, depth, elementsList, hasLoaded } = useThreeDCloud({
+    children,
+    size,
+    speed,
+    radius,
+  });
 
   return (
     <div className="threed-cloud-container">
--- src/hooks/useThreeDCloud.tsx
+++ src/hooks/useThreeDCloud.tsx
@@ -1,11 +1,30 @@
-import React from 'react';
-import { CloudProps } from '../components/types';
+import React, { PropsWithChildren, useLayoutEffect, useState } from 'react';
+import { CloudProps, Position } from '../components/types';
 
 export function useThreeDCloud({
-  size = 100,
-  speed = 1,
-  radius = 150,
-}: Partial<CloudProps>) {
+  children = [],
+  size,
+  speed,
+  radius,
+}: PropsWithChildren<CloudProps>) {
+  const elements: Array<JSX.Element> = React.Children.toArray(
+    children
+  ) as Array<JSX.Element>;
+
+  const [hasLoaded, setHasLoaded] = useState<boolean>(false);
+  const [elementsList, setElementsList] = useState<Array<CloudProps>>(
+    elements.map((element) => ({
+      item: element!.props.children,
+      radius,
+      size,
+      speed,
+    }))
+  );
+
+  const elementRandomPosition: Array<Number> = elements.map(
+    (_element, index) => index
+  );
+
   const mounted = React.useRef(true);
 
   // Direction
@@ -22,9 +41,64 @@ export function useThreeDCloud({
   // rolling depth
   const depth = 1.5 * radius;
 
+  function createItems() {
+    const cloudElements = elementsList.map((element, index) => ({
+      item: element.item,
+      radius,
+      position: computePosition(index),
+      size,
+      speed,
+    }));
+
+    setElementsList(cloudElements);
+    setHasLoaded(true);
+  }
+
+  function computePosition(index: number, random = true): Position {
+    const textsLength = elements.length;
+
+    if (random)
+      index = getRandomPosition(
+        Math.floor(Math.random() * (textsLength + 1))
+      ).valueOf();
+    const theta = Math.acos(-1 + (2 * index + 1) / textsLength);
+    const phi = Math.sqrt((textsLength + 1) * Math.PI) * theta;
+
+    return {
+      x: (size * Math.sin(theta) * Math.cos(phi)) / 2,
+      y: (size * Math.sin(theta) * Math.sin(phi)) / 2,
+      z: (size * Math.cos(theta)) / 2,
+    };
+  }
+
+  function getRandomPosition(index: number): Number {
+    const closest = elementRandomPosition.reduce(
+      (previousValue: Number, currentValue: Number): Number =>
+        Math.abs(currentValue.valueOf() - index) <
+        Math.abs(previousValue.valueOf() - index)
+          ? currentValue
+          : previousValue
+    );
+    for (let i = 0; i < elementRandomPosition.length; i++) {
+      if (elementRandomPosition[i] === closest) {
+        elementRandomPosition.splice(i, 1);
+      }
+    }
+    return closest;
+  }
+
+  useLayoutEffect(() => {
+    if (mounted.current) {
+      createItems();
+      mounted.current = false;
+    }
+  }, []);
+
   return {
-    mounted,
+    elements,
     depth,
     sc,
+    elementsList,
+    hasLoaded,
   };
 }
