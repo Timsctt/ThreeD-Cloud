86d1fdd06e7eed5323dcae662632540335cfda6b
Timsht <38407598+Timsht@users.noreply.github.com>
2023-02-19 16:31:23 +0100

    test: check className customization

--- tests/ThreeDCloud.test.tsx
+++ tests/ThreeDCloud.test.tsx
@@ -4,18 +4,18 @@ import CloudElement from '../src/components/ThreeDCloud/CloudElement';
 import '@testing-library/jest-dom/extend-expect';
 
 describe('displayCloud', () => {
-  test('renders the component with elements', () => {
-    const elements = [
-      { item: <span>Element 1</span>, position: { x: 0, y: 0, z: 0 } },
-      { item: <span>Element 2</span>, position: { x: 100, y: 100, z: 0 } },
-      { item: <span>Element 3</span>, position: { x: -100, y: -100, z: 0 } },
-    ];
-    const radius = 200;
-    const size = 200;
-    const speed = 1;
-    const depth = size * 1.5;
-    const sc: number[] = [];
+  const elements = [
+    { item: <span>Element 1</span>, position: { x: 0, y: 0, z: 0 } },
+    { item: <span>Element 2</span>, position: { x: 100, y: 100, z: 0 } },
+    { item: <span>Element 3</span>, position: { x: -100, y: -100, z: 0 } },
+  ];
+  const radius = 200;
+  const size = 200;
+  const speed = 1;
+  const depth = size * 1.5;
+  const sc: number[] = [];
 
+  test('renders the component with elements', () => {
     render(
       <CloudContainer radius={radius} speed={speed} size={size}>
         {elements.map((element, index) => (
@@ -38,5 +38,30 @@ describe('displayCloud', () => {
     expect(element1).toBeInTheDocument();
     expect(element2).toBeInTheDocument();
     expect(element3).toBeInTheDocument();
+  });
+
+  test('test custom className', () => {
+    const { container } = render(
+      <CloudContainer
+        radius={radius}
+        speed={speed}
+        size={size}
+        className="red rounded"
+      >
+        {elements.map((element, index) => (
+          <CloudElement
+            key={index}
+            position={element.position}
+            sc={sc}
+            depth={depth}
+          >
+            {element.item}
+          </CloudElement>
+        ))}
+      </CloudContainer>
+    );
+
+    expect(container.firstChild?.firstChild).toHaveClass('red');
+    expect(container.firstChild?.firstChild).toHaveClass('rounded');
   });
 });
