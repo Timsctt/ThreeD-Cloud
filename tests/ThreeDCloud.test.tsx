d930d0a78e5f2f97b388e998e920cf4ae2fbe590
Timsht <38407598+Timsht@users.noreply.github.com>
2023-02-19 02:09:47 +0100

    test: check elements are right displayed

--- tests/ThreeDCloud.test.tsx
+++ tests/ThreeDCloud.test.tsx
@@ -1,13 +1,42 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { ThreeDCloud } from '../src';
+import CloudElement from '../src/components/ThreeDCloud/CloudElement';
+import '@testing-library/jest-dom/extend-expect';
 
 describe('displayCloud', () => {
-  it('should render the cloud', () => {
+  test('renders the component with elements', () => {
+    const elements = [
+      { item: <span>Element 1</span>, position: { x: 0, y: 0, z: 0 } },
+      { item: <span>Element 2</span>, position: { x: 100, y: 100, z: 0 } },
+      { item: <span>Element 3</span>, position: { x: -100, y: -100, z: 0 } },
+    ];
+    const radius = 200;
+    const size = 200;
+    const speed = 1;
+    const depth = size * 1.5;
+    const sc: number[] = [];
+
     render(
-      <ThreeDCloud radius={200} size={150} speed={0.6}>
-        <p>element</p>
-        <p>element</p>
+      <ThreeDCloud radius={radius} speed={speed} size={size}>
+        {elements.map((element, index) => (
+          <CloudElement
+            key={index}
+            initialPos={element.position}
+            sc={sc}
+            depth={depth}
+          >
+            {element.item}
+          </CloudElement>
+        ))}
       </ThreeDCloud>
     );
+
+    const element1 = screen.getByText('Element 1');
+    const element2 = screen.getByText('Element 2');
+    const element3 = screen.getByText('Element 3');
+
+    expect(element1).toBeInTheDocument();
+    expect(element2).toBeInTheDocument();
+    expect(element3).toBeInTheDocument();
   });
 });
