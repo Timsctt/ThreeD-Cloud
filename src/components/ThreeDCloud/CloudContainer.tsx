e8b1540a83a6e5addd04c8f6aa6bfb90f60f4458
Timsht <38407598+Timsht@users.noreply.github.com>
2023-03-22 23:35:31 +0100

    feat: conditionally display icons

--- src/components/ThreeDCloud/CloudContainer.tsx
+++ src/components/ThreeDCloud/CloudContainer.tsx
@@ -8,17 +8,32 @@ import './style.css';
 export const CloudContainer: React.FunctionComponent<CloudContainerProps> = (
   props: CloudContainerProps
 ) => {
-  const { sc, depth, elementsList, handlePause, pause, handlePauseByKey } =
-    useCloudContainer(props);
+  const {
+    sc,
+    depth,
+    elementsList,
+    handlePause,
+    pause,
+    handlePauseByKey,
+    isPausable,
+    iconOnHover,
+  } = useCloudContainer(props);
 
   return (
     <div
-      className="threed-cloud-container"
+      className={`threed-cloud-container   ${
+        isPausable ? 'tdcIsPausable' : ''
+      }`}
       role="button"
       onClick={handlePause}
       onKeyDown={handlePauseByKey}
       tabIndex={0}
     >
+      {isPausable && iconOnHover && (
+        <span
+          className={`${pause ? 'tdc-animation-pause' : 'tdc-animation-play'}`}
+        />
+      )}
       <div
         className={props.className}
         style={{
